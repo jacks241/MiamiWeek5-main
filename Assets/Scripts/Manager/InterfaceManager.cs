25485f71ec1ba34d0607126d62e7dc5ed33df5a9
jacks241 <jacks241@miamioh.edu>
2021-10-06 23:10:26 -0400

    .

---
+++ Assets/Scripts/Manager/InterfaceManager.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class InterfaceManager : MonoBehaviour
+{
+    [SerializeField]
+    private Button joinPlayerOne;
+    [SerializeField]
+    private Button addPlayerTwo;
+    Text text1;
+    Text text2;
+    
+
+    //TODO: Add PlayerTwoButton reference
+
+    [SerializeField]
+    private SplitKeyboardPlayerInputManager playerInputManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        joinPlayerOne.onClick.AddListener(() => JoinPlayerOne());
+        addPlayerTwo.onClick.AddListener(() => JoinPlayerTwo());
+        //TODO Listen for player two click event
+    }
+
+    private void JoinPlayerOne()
+    {
+        playerInputManager.JoinPlayer(0, "Keyboard&Mouse");
+        
+        text1 = joinPlayerOne.GetComponentInChildren<Text>();
+        text1.text = "Leave Player One";
+
+        //TODO flip text to say "Leave Player One"
+        //TODO on click after player has joined, remove player
+    }
+
+    private void JoinPlayerTwo()
+    {
+        playerInputManager.JoinPlayer(1, "PlayerTwo");
+        text2 = addPlayerTwo.GetComponentInChildren<Text>();
+        text2.text = "Leave Player Two";
+    }
+
+    //TODO Invoke JoinPlayer passing a playerIndex value and targeting a control scheme
+    //TODO flip text after player has joined to say "Leave Player Two"
+    //TODO on click after player has joined, remove player
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public enum PlayerNumber { PlayerOne, PlayerTwo }
+    [SerializeField]
+    private Rigidbody rigidbody;
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private PlayerInput playerInput;
+    [SerializeField]
+    private MeshRenderer meshRenderer;
+
+    private Vector3 direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(transform.position + (direction * speed * Time.deltaTime));
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Debug.Log("Move: " + value.Get<Vector2>());
+        var dir = value.Get<Vector2>();
+        direction.x = dir.x;
+        direction.z = dir.y;
+    }
+
+    public void SetPlayer(PlayerNumber playerNumber)
+    {
+        if (playerNumber == PlayerNumber.PlayerOne)
+        {
+            meshRenderer.material.color = Color.blue;
+        }
+
+        if (playerNumber == PlayerNumber.PlayerTwo)
+        {
+            meshRenderer.material.color = Color.red;
+        }
+        //TODO change player two color
+    }
+}
